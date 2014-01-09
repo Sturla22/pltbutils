38b0b26d765c92e1e6859878bfb2c94e3c76712d
pela <pela@7b40e9c4-bce6-4288-896c-3da371718cce>
2014-01-09 20:49:33 +0000

    alpha0006 - tb_template.vhd: Updated for the new status- and control signal and variable pltbv and pltbs.

--- template/vhdl/tb_template.vhd
+++ template/vhdl/tb_template.vhd
@@ -26,7 +26,7 @@
 ----                                                              ----
 ----------------------------------------------------------------------
 ----                                                              ----
----- Copyright (C) 2013 Authors and OPENCORES.ORG                 ----
+---- Copyright (C) 2013-2014 Authors and OPENCORES.ORG            ----
 ----                                                              ----
 ---- This source file may be used and distributed without         ----
 ---- restriction provided that this copyright statement is not    ----
@@ -67,17 +67,8 @@ end entity tb_template;
 architecture bhv of tb_template is
 
   -- Simulation status- and control signals
-  signal test_num       : integer;
-  -- VHDL-1993:
-  --signal test_name      : string(pltbutils_test_name'range);
-  --signal info           : string(pltbutils_info'range);
-  -- VHDL-2002:
-  signal test_name      : string(pltbutils_sc.test_name'range);
-  signal info           : string(pltbutils_sc.info'range);
-
-  signal checks         : integer;
-  signal errors         : integer;
-  signal stop_sim       : std_logic;
+  -- for accessing .stop_sim and for viewing in waveform window
+  signal pltbs          : pltbs_t := C_PLTBS_INIT;
   
   -- DUT stimuli and response signals
   signal clk            : std_logic;
@@ -85,21 +76,6 @@ architecture bhv of tb_template is
   -- < Template info: add more DUT stimuli and response signals here. >
   
 begin
-
-  -- Simulation status and control for viewing in waveform window
-  -- VHDL-1993:
-  --test_num  <= pltbutils_test_num;
-  --test_name <= pltbutils_test_name;
-  --checks    <= pltbutils_chk_cnt;
-  --errors    <= pltbutils_err_cnt;
-  -- VHDL-2002:
-  test_num  <= pltbutils_sc.test_num;
-  test_name <= pltbutils_sc.test_name;
-  info      <= pltbutils_sc.info;
-  checks    <= pltbutils_sc.chk_cnt;
-  errors    <= pltbutils_sc.err_cnt;
-  stop_sim  <= pltbutils_sc.stop_sim;
-  
 
   dut0 : entity work.template
     generic map (
@@ -117,7 +93,7 @@ begin
     )
     port map(
       clk_o             => clk,
-      stop_sim_i        => stop_sim
+      stop_sim_i        => pltbs.stop_sim
     );
    
   tc0 : entity work.tc_example
