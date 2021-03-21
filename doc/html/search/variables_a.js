84340f50caab7e6d7f266c467bea19f336487bea
Sturla Lange <sturla22@gmail.com>
2021-03-21 20:15:05 +0100

    move html

--- doc/html/search/variables_a.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['y_357',['y',['../classdut__example_1_1rtl.html#a61e5e7a4c2ca266e366682daeb85bb92',1,'dut_example.rtl.y()'],['../classtb__example1_1_1bhv.html#a70e68dd99e9bab52dfa4c3bddb370bc9',1,'tb_example1.bhv.y()'],['../classtb__example2_1_1bhv.html#a70e68dd99e9bab52dfa4c3bddb370bc9',1,'tb_example2.bhv.y()'],['../classtc__example2.html#a9df30f532a9df0ce4c7653558c77c68b',1,'tc_example2.y()']]],
-  ['y_5fi_358',['y_i',['../classdut__example.html#a262bf3ac9f72fc74df66c1e920f884e0',1,'dut_example']]]
-];
